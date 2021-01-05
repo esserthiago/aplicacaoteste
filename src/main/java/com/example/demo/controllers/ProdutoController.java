c11f03d4d105bf4d29acc020dad21bf905072a36
fesserm <38021759+fesserm@users.noreply.github.com>
2021-01-04 23:16:10 -0200

    teste
    Teste

---
+++ src/main/java/com/example/demo/controllers/ProdutoController.java
@@ -0,0 +1,28 @@
+package com.example.demo.controllers;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.model.entities.Produto;
+import com.example.demo.model.repositories.ProdutoRepository;
+
+@RestController
+@RequestMapping(path="/api/produtos")
+public class ProdutoController {
+	
+	@Autowired
+	private ProdutoRepository produtoRepository;
+
+	@PostMapping
+	public @ResponseBody Produto novoProduto(@Valid Produto produto){
+		
+		produtoRepository.save(produto);
+		
+		return produto;
+	}
+}
